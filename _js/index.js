31f0725f9cade883fa3fd01c04726f3eb44347c7
Étienne Després <etienne@pochour.com>
2017-05-26 13:21:27 -0400

    Utilise NPM et webpack

---
+++ _js/index.js
@@ -0,0 +1,38 @@
+import Pjax from 'pjax';
+import smoothScroll from 'smooth-scroll';
+import Player from '@vimeo/player';
+
+document.addEventListener("pjax:complete", function() {
+  var slug = document.querySelector('article.content').dataset.bodyClass || '';
+  document.body.className = slug;
+  
+  var currentProject = document.querySelector('article.current');
+  
+  if (currentProject) {
+    currentProject.classList.remove('current');
+  }
+  
+  if (slug) {
+    document.querySelector('article.' + slug).classList.add('current');
+  }
+})
+
+new Pjax({ selectors: ['title', 'article.content'] });
+
+smoothScroll.init({ speed: 400 });
+
+var videos = document.getElementsByClassName('video');
+
+for(var i = 0; i < videos.length; i++) {
+  var video = videos.item(i);
+
+  if (typeof window.orientation !== 'undefined') {
+    initializeVideo.bind(video)();
+  } else {
+    video.onclick = initializeVideo;
+  }
+}
+
+function initializeVideo() {
+  var player = new Player(this, {autoplay: true});
+}
