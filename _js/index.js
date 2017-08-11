4c281fdd4782730b2e76260c1abab038dd1968ce
Étienne Després <etienne@pochour.com>
2017-08-11 14:58:55 -0400

    Répare le piton cycle. Fixes #10

--- _js/index.js
+++ _js/index.js
@@ -4,26 +4,47 @@ import Player from '@vimeo/player';
 
 // Home
 
-var cycleButton = document.getElementById('cycle-button');
-if (cycleButton) {
-  cycleButton.onclick = function() {
-    var currentGroup = parseInt(this.dataset.currentGroup);
-    var totalGroups = parseInt(document.querySelector('.cycle span:last-child').dataset.group);
-    var nextGroup = currentGroup + 1 > totalGroups ? 1 : currentGroup + 1;
-    document.querySelectorAll('.cycle span').forEach(function(element) {
-      element.classList.add('hide');
-    });
-    document.querySelectorAll(".cycle span[data-group='" + nextGroup + "']").forEach(function(element) {
-      element.classList.remove('hide');
-    });
-    this.dataset.currentGroup = nextGroup;
-    return false;
-  };
+function domReady() {
+  // Cycle button
+  var cycleButton = document.getElementById('cycle-button');
+  if (cycleButton) {
+    cycleButton.onclick = function() {
+      var currentGroup = parseInt(this.dataset.currentGroup);
+      var totalGroups = parseInt(document.querySelector('.cycle span:last-child').dataset.group);
+      var nextGroup = currentGroup + 1 > totalGroups ? 1 : currentGroup + 1;
+      document.querySelectorAll('.cycle span').forEach(function(element) {
+        element.classList.add('hide');
+      });
+      document.querySelectorAll(".cycle span[data-group='" + nextGroup + "']").forEach(function(element) {
+        element.classList.remove('hide');
+      });
+      this.dataset.currentGroup = nextGroup;
+      return false;
+    };
+  }
+  
+  // Videos
+  var videos = document.getElementsByClassName('video');
+
+  for(var i = 0; i < videos.length; i++) {
+    var video = videos.item(i);
+
+    if (typeof window.orientation !== 'undefined') {
+      initializeVideo.bind(video)();
+    } else {
+      video.onclick = initializeVideo;
+    }
+  }
+
+  function initializeVideo() {
+    var player = new Player(this, {autoplay: true});
+  }
 }
 
 // Pjax
 
 document.addEventListener("pjax:success", function() {
+  domReady();
   var slug = document.querySelector('article.content').dataset.bodyClass || 'home';
   document.body.className = slug;
 
@@ -39,26 +60,12 @@ document.addEventListener("pjax:success", function() {
 
 new Pjax({
   debug: false,
+  elements: ['a[href]:not(#cycle-button)'],
   selectors: ['title', 'header#menu', 'article.content']
 });
 
 // Smooth scroll
 var smoothScroll = new SmoothScroll('a[href*="#"]', { speed: 400 });
 
-// Videos
-
-var videos = document.getElementsByClassName('video');
-
-for(var i = 0; i < videos.length; i++) {
-  var video = videos.item(i);
-
-  if (typeof window.orientation !== 'undefined') {
-    initializeVideo.bind(video)();
-  } else {
-    video.onclick = initializeVideo;
-  }
-}
-
-function initializeVideo() {
-  var player = new Player(this, {autoplay: true});
-}
+// Initialize
+domReady();
