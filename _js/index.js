3355b312adfbeb06aa2a692db1485ac339a4c421
Étienne Després <etienne@pochour.com>
2017-09-15 10:16:52 -0400

    Remove Pjax

--- _js/index.js
+++ _js/index.js
@@ -1,7 +1,5 @@
-import Pjax from 'pjax';
 import SmoothScroll from 'smooth-scroll';
 import Player from '@vimeo/player';
-import topbar from 'topbar';
 
 // Home
 
@@ -46,51 +44,12 @@ function domReady() {
   document.querySelector('article.content').tabIndex = -1;
 }
 
-// Pjax
-
-document.addEventListener("pjax:send", function() {
-  loadStart = Date.now();
-  topbar.show();
-})
-
-document.addEventListener("pjax:success", function() {
-  topbar.hide();
-  domReady();
-  var slug = document.querySelector('article.content').dataset.bodyClass || 'home';
-  document.body.className = slug;
-
-  // Only used for menu links on the About page; slightly kludgy
-  var hash = document.location.hash;
-  if (hash.length > 0) {
-    document.querySelector('a[href="' + hash + '"]').click();
-  }
-  
-  // We need to manually change the document language
-  document.querySelector('html').lang = document.querySelector('article.content').dataset.lang;
-  
-  // Move focus to main article
-  document.querySelector('article.content').focus();
-});
-
 function updatePiwik() {
   _paq.push(['setCustomUrl', location.pathname]);
   _paq.push(['setDocumentTitle', document.title]);
   _paq.push(['setGenerationTimeMs', Date.now() - loadStart]);
   _paq.push(['trackPageView']);
 }
-
-new Pjax({
-  analytics: updatePiwik,
-  debug: false,
-  elements: ['a[href]:not(#cycle-button)'],
-  selectors: ['title', 'meta', 'header#menu', 'article.content', 'section.projects', 'footer']
-});
-
-// Topbar
-
-topbar.config({
-  barColors: { '0': 'white', '1.0': 'white' },
-});
 
 // Smooth scroll
 var smoothScroll = new SmoothScroll('a[href*="#"]', { speed: 400 });
